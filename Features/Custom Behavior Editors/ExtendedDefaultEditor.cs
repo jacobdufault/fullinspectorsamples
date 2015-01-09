1844138159b6f74a8bf31e295d2a44c06eb47f35
Jacob Dufault <jacobdufault@gmail.com>
2015-01-08 23:08:29 -0600

    A bit of directory reorg, make `other` samples use FullSerializer

---
+++ Features/Custom Behavior Editors/ExtendedDefaultEditor.cs
@@ -0,0 +1,5 @@
+﻿namespace FullInspector.Samples {
+    public class ExtendedDefaultEditor : BaseBehavior<FullSerializerSerializer> {
+        public int fromDefaultEditor1, fromDefaultEditor2, fromDefaultEditor3;
+    }
+}
