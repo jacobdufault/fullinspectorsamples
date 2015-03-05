fa8a7c3ff377259256e4e93d09b1828aa21a236a
Jacob Dufault <jacobdufault@gmail.com>
2015-03-05 14:40:21 -0500

    Update samples

--- Features/Custom Behavior Editors/ExtendedDefaultEditor.cs
+++ Features/Custom Behavior Editors/ExtendedDefaultEditor.cs
@@ -1,4 +1,7 @@
-﻿namespace FullInspector.Samples {
+﻿using UnityEngine;
+
+namespace FullInspector.Samples {
+    [AddComponentMenu("Full Inspector Samples/Other/Extended Default Editor")]
     public class ExtendedDefaultEditor : BaseBehavior<FullSerializerSerializer> {
         public int fromDefaultEditor1, fromDefaultEditor2, fromDefaultEditor3;
     }
